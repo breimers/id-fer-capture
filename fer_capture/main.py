8a440a4d02452d9d8cc169386b88cafc6069a9c7
Bradley <bradley@introspectdata.com>
2020-01-20 18:01:49 -0800

    rework

--- fer_capture/main.py
+++ fer_capture/main.py
@@ -1,25 +1,37 @@
-import base64
-import json
-import os
-import sys
-import uuid
-
-import click
-import magic
 import logging
+import os
 
 import cv2
 import numpy as np
-from keras.models import load_model
-from tensorflow.compat.v1 import ConfigProto, InteractiveSession
-from tensorflow import get_logger
 
+import tensorflow as tf
 
 #logging
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger("fer-capture-log")
 
-def face_check(image_path, model_path):
+#global values
+emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
+face_casc = os.path.join(os.path.dirname(__file__), "haarcascade_frontalface_default.xml")
+
+def path_to_img(image_path):
+    return cv2.imread(image_path)
+
+def detect_faces(frame):
+    face_cascade = cv2.CascadeClassifier(face_casc)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    log.info("Beginning facial detection.")
+    faces = face_cascade.detectMultiScale(
+        frame,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(48, 48),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    log.info("Detected {} faces.".format(len(faces)))
+    return faces
+
+def face_check(img, model, show=False):
     """
     This function performs an FER routine on the given image using the specified model.
 
@@ -28,82 +40,70 @@ def face_check(image_path, model_path):
         model_path (str): Valid file path to an h5 model.
 
     Returns:
-        data (dict): Dictionary containing the base64+ut8 encoding of the detected faces along with the predicted emotion.
+        data (dict): Dictionary containing detected faces along with the predicted emotion.
     """
-    #tensorflow setup
-    config = ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
-    get_logger().setLevel('ERROR')
     #begin analysis
-    emotion_dict = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
-    face_casc = os.path.join(os.path.dirname(__file__), "haarcascade_frontalface_default.xml")
-    model = load_model(model_path)
-    log.info("Loaded model: {}".format(model_path))
-    face_cascade = cv2.CascadeClassifier(face_casc)
-    log.info("Loaded cv2 cascade: {}".format(face_casc))
-    log.info("Preparing image for processing: {}".format(image_path))
-    frame = cv2.imread(image_path)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    log.info("Beginning facial detection.")
-    faces = face_cascade.detectMultiScale(
-        frame,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(48, 48),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-    log.info("Detected {} faces.".format(len(faces)))
-    data = {"id" : str(uuid.uuid4()), "faces" : []}
+    frame = img
+    faces = detect_faces(frame)
+    data = {"faces" : []}
     log.info("Beginning emotion recognition.")
-    for (x, y, w, h) in faces:
+
+    for i, (x, y, w, h) in enumerate(faces):
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
         cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
-        prediction = model.predict(cropped_img)
-        retval, buffer = cv2.imencode('.jpg', roi_gray)
-        img_as_text = base64.b64encode(buffer)
+        prediction = model.predict(cropped_img)[0]
+        predictions = {
+            "angry": float(prediction[0]),
+            "disgusted": float(prediction[1]),
+            "fearful": float(prediction[2]),
+            "happy": float(prediction[3]),
+            "sad": float(prediction[4]),
+            "surprised": float(prediction[5]),
+            "neutral": float(prediction[6])
+        }
         data["faces"].append({
-            "id" : str(uuid.uuid4()),
-            "as_b64_utf8_str" : str(img_as_text, "utf-8"),
-            "prediction" : {str(int(np.argmax(prediction))) : emotion_dict[int(np.argmax(prediction))]}
+            "id" : i,
+            "xywh": (x, y, w, h),
+            "predictions": predictions
         })
+        if show:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 4)
+            cv2.putText(
+                frame,
+                "id: {}  |  {}: {}".format(
+                    i,
+                    max(predictions, key=predictions.get),
+                    round(
+                        predictions[
+                            max(predictions, key=predictions.get)
+                        ],
+                        3
+                    )
+                ),
+                (x, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (36,255,12),
+                2
+            )
     log.info("Detection completed successfully!")
+    if show:
+        cv2.imshow('Facial Detection', frame)
+        cv2.waitKey(0)
     return data
 
-@click.command()
-@click.option("--model", default = "model.h5", help = "Path to model binary.")
-@click.option("--image", help = "Path to JPEG image.")
-@click.option("--out", default = "raw", help = " 'raw': print dictionary to stdout, 'json': json to file ")
-def cli(model, image, out):
-    """
-    This function acts as the interface between the command-line and the face_check function.
+def check_image(image_path, model_path, show=False):
+    img = path_to_img(image_path)
+    model = tf.keras.models.load_model(model_path)
+    return face_check(img, model, show)
 
-    Parameters:
-        image (str): Valid file path to a JPEG image.
-        model (str): Valid file path to an h5 model.
-        out (str): 'raw' or 'json'
-
-    Returns:
-        data (dict): Dictionary containing the base64+ut8 encoding of the detected faces along with the predicted emotion.
-        OR
-        data (json): JSON containing the base64+ut8 encoding of the detected faces along with the predicted emotion
-    """
-    mime = magic.Magic(mime=True)
-    if not mime.from_file(model) == "application/x-hdf":
-        log.error("{} is not a valid application/x-hdf!".format(model))
-        return 1
-    if not mime.from_file(image) == "image/jpeg":
-        log.error("{} is not a valid image/jpeg!".format(image))
-        return 1
-    try:
-        data = face_check(image, model)
-    except Exception as e:
-        log.error(e)
-        return 1
-    if out == "json":
-        with open(str(image.split("/")[-1].replace(".", "-") + ".json"), "w") as f:
-            f.write(json.dumps(data))
-            return 0
-    print(data)
+def check_stream(input=0, model_path, show=False):
+    cap = cv2.VideoCapture(input)
+    model = tf.keras.models.load_model(model_path)
+    data = []
+    while True:
+        _, img = cap.read()
+        data.append(face_check(img, model, show))
+    cap.release()
     return data
